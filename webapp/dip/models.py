4abc0e29f54ee34dd3566d6d604fcb2eb777aeb0
Lokesh Balani <lokeshbalani1@outlook.com>
2018-10-27 19:26:29 +0530

    Created stub for image upload

--- webapp/dip/models.py
+++ webapp/dip/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    image = models.FileField(upload_to='image/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- webapp/dip/views.py
+++ webapp/dip/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.views import generic
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from .forms import ImageForm
 
 # Create your views here.
 def index(request):
@@ -14,3 +17,30 @@ def histogram_plot_view(request):
     template_name = 'modules/histogram/hist-plt.html'  
 
     return render(request, template_name)
+
+def image_upload(request):
+    if request.method == 'POST' and request.FILES['usr-uploaded-img']:
+        usr_img = request.FILES['usr-uploaded-img']
+        fs = FileSystemStorage()
+        filename = fs.save(usr_img.name, usr_img)
+        uploaded_image_url = fs.url(filename)
+
+        template_name = 'modules/histogram/hist-plt.html'
+        return render(request, template_name, {
+            'uploaded_image_url': uploaded_image_url
+        })
+
+def model_form_upload(request):
+    template_name = 'modules/histogram/hist-plt.html'
+
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('histplt')
+    else:
+        form = ImageForm()
+
+    return render(request, template_name, {
+        'form': form
+    })
