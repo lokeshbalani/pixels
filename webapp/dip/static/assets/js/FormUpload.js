395da0a9fdcfa6518d69246465329171aae4005c
Lokesh Balani <lokeshbalani1@outlook.com>
2018-11-10 23:51:46 +0530

    Fixed issue: images not submitted when dropped

--- webapp/dip/static/assets/js/FormUpload.js
+++ webapp/dip/static/assets/js/FormUpload.js
@@ -9,9 +9,10 @@
 
 
     // applying the effect for every form
-    var forms = document.querySelectorAll('.box');
+    var forms = document.querySelectorAll('form');
     Array.prototype.forEach.call(forms, function (form) {
-        var input = form.querySelector('input[type="file"]'),
+        var box = form.querySelector('.box'),
+            input = form.querySelector('input[type="file"]'),
             label = form.querySelector('label'),
             errorMsg = form.querySelector('.box__error span'),
             restart = form.querySelectorAll('.box__restart'),
@@ -22,11 +23,15 @@
                 // label.style.display = 'none';
                 var placeholder = target.parentElement;
                 placeholder.querySelector('.box__icon').style.display = 'none';
-                placeholder.parentElement.classList.remove('box__padding');
-                
+                if (target.classList.contains("box__padding")) {
+                    target.classList.remove('box__padding');
+                } else {
+                    placeholder.parentElement.classList.remove('box__padding');
+                }
+
                 var uploadedDisplayEl = placeholder.querySelector('.box__im__uploaded');
                 uploadedDisplayEl.innerHTML = "";
-                
+
                 Array.prototype.forEach.call(files, function (file, index) {
                     var img = document.createElement('img');
                     img.setAttribute("class", "photo")
@@ -36,9 +41,9 @@
                     img.style.width = '100%';
                     img.src = window.URL.createObjectURL(file);
                     uploadedDisplayEl.append(img);
-                    px.utils.generate_zoom_pan_img('.px-img')
+                    px.utils.generate_zoom_pan_img(img)
                 });
-            
+
             },
             triggerFormSubmit = function () {
                 var event = document.createEvent('HTMLEvents');
@@ -60,7 +65,7 @@
 
         // drag&drop files if the feature is available
         if (isAdvancedUpload) {
-            form.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
+            box.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
 
             ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function (event) {
                 form.addEventListener(event, function (e) {
@@ -71,90 +76,26 @@
             });
             ['dragover', 'dragenter'].forEach(function (event) {
                 form.addEventListener(event, function () {
-                    form.classList.add('is-dragover');
+                    box.classList.add('is-dragover');
                 });
             });
             ['dragleave', 'dragend', 'drop'].forEach(function (event) {
                 form.addEventListener(event, function () {
-                    form.classList.remove('is-dragover');
+                    box.classList.remove('is-dragover');
                 });
             });
-            form.addEventListener('drop', function (e) {
+            box.addEventListener('drop', function (e) {
                 droppedFiles = e.dataTransfer.files; // the files that were dropped
+                input.files = droppedFiles;
                 showFiles(droppedFiles, e.target.parentElement);
             });
         }
-
-
-        // if the form was submitted
-        form.addEventListener('submit', function (e) {
-            // preventing the duplicate submissions if the current one is in progress
-            if (form.classList.contains('is-uploading')) return false;
-
-            form.classList.add('is-uploading');
-            form.classList.remove('is-error');
-
-            if (isAdvancedUpload) // ajax file upload for modern browsers
-            {
-                e.preventDefault();
-
-                // gathering the form data
-                var ajaxData = new FormData(form);
-                if (droppedFiles) {
-                    Array.prototype.forEach.call(droppedFiles, function (file) {
-                        ajaxData.append(input.getAttribute('name'), file);
-                    });
-                }
-
-                // ajax request
-                var ajax = new XMLHttpRequest();
-                ajax.open(form.getAttribute('method'), form.getAttribute('action'), true);
-
-                ajax.onload = function () {
-                    form.classList.remove('is-uploading');
-                    if (ajax.status >= 200 && ajax.status < 400) {
-                        var data = JSON.parse(ajax.responseText);
-                        form.classList.add(data.success == true ? 'is-success' : 'is-error');
-                        if (!data.success) errorMsg.textContent = data.error;
-                    } else alert('Error. Please, contact the webmaster!');
-                };
-
-                ajax.onerror = function () {
-                    form.classList.remove('is-uploading');
-                    alert('Error. Please, try again!');
-                };
-
-                ajax.send(ajaxData);
-            } else // fallback Ajax solution upload for older browsers
-            {
-                var iframeName = 'uploadiframe' + new Date().getTime(),
-                    iframe = document.createElement('iframe');
-
-                $iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
-
-                iframe.setAttribute('name', iframeName);
-                iframe.style.display = 'none';
-
-                document.body.appendChild(iframe);
-                form.setAttribute('target', iframeName);
-
-                iframe.addEventListener('load', function () {
-                    var data = JSON.parse(iframe.contentDocument.body.innerHTML);
-                    form.classList.remove('is-uploading')
-                    form.classList.add(data.success == true ? 'is-success' : 'is-error')
-                    form.removeAttribute('target');
-                    if (!data.success) errorMsg.textContent = data.error;
-                    iframe.parentNode.removeChild(iframe);
-                });
-            }
-        });
-
 
         // restart the form if has a state of error/success
         Array.prototype.forEach.call(restart, function (entry) {
             entry.addEventListener('click', function (e) {
                 e.preventDefault();
-                form.classList.remove('is-error', 'is-success');
+                box.classList.remove('is-error', 'is-success');
                 input.click();
             });
         });
