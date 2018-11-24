be886e20275f725100f52d76a222abeaedceb48a
Lokesh Balani <lokeshbalani1@outlook.com>
2018-11-25 03:06:30 +0530

    Cleaned up the project tree

--- spikes/python-libs/.ipynb_checkpoints/opencv-test-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-from matplotlib import pyplot as plt
-
-img = cv2.imread('./')
-print(img)
-#plt.imshow(img, cmap = 'gray')
-#plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-#plt.show()
